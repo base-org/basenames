69d17e28558d1d74298ae6b332e968bcda32440a
katzman <steve.katzman@coinbase.com>
2024-05-09 14:05:12 -0700

    Fix pragmas

--- contracts/src/L2/Registrar.sol
+++ contracts/src/L2/Registrar.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {BaseRegistrar} from "./BaseRegistrar.sol";
 import {ReverseRegistrar} from "./ReverseRegistrar.sol";
--- contracts/src/L2/Registry.sol
+++ contracts/src/L2/Registry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {ENSRegistry} from "ens-contracts/registry/ENSRegistry.sol";
 
