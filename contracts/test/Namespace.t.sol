793a6322d77c417a870520b78262cc9f8f671563
katzman <steve.katzman@coinbase.com>
2024-05-09 15:59:23 -0700

    Rebase tests

--- contracts/test/Namespace.t.sol
+++ contracts/test/Namespace.t.sol
@@ -6,7 +6,6 @@ import {console2} from "forge-std/console2.sol";
 import {NameEncoder} from "ens-contracts/utils/NameEncoder.sol";
 
 contract Namespace is Test {
-
     function test_encodeName() public pure {
         (bytes memory dnsName, bytes32 node) = NameEncoder.dnsEncodeName("base.eth");
         console2.logBytes(dnsName);
