ada266b0ef0366d1a4d31f610e35e0e68e69869e
katzman <steve.katzman@coinbase.com>
2024-05-14 10:52:45 -0700

    Add script to set URL

--- contracts/script/configure/SetL1ResolverUrl.s.sol
+++ contracts/script/configure/SetL1ResolverUrl.s.sol
@@ -7,18 +7,13 @@ import "src/L1/L1Resolver.sol";
 contract SetL1ResolverUrl is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddresss = vm.addr(deployerPrivateKey);
+        address resolverAddress = 0x5F15c3B5949F5767F5Ca9013a8E4Ca4D97a053eD;
         vm.startBroadcast(deployerPrivateKey);
 
-        /// L1 Resolver constructor data
-        string memory url =
-            "http://localhost:8000/api/v1/domain/resolver/resolveDomain/{sender}/{data}"; //
-        address[] memory signers = new address[](1);
-        signers[0] = 0xa412c16ECd2198A6aBce8235651E105684Fb77ed; // DEV signer
-        address owner = deployerAddresss;
+        string memory NEW_URL = "";
 
-        L1Resolver l1 = new L1Resolver(url, signers, owner);
-        console.log(address(l1));
+        L1Resolver resolver = L1Resolver(resolverAddress);
+        resolver.setUrl(NEW_URL);
 
         vm.stopBroadcast();
     }
