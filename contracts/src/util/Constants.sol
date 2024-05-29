cede78c752048dc08c7ef4f10df29a6c9f567df3
katzman <steve.katzman@coinbase.com>
2024-05-29 14:39:40 -0700

    Removed SHA3_LOOKUP from constants

--- contracts/src/util/Constants.sol
+++ contracts/src/util/Constants.sol
@@ -9,7 +9,6 @@ bytes32 constant BASE_ETH_NODE = 0xff1e3c0eb00ec714e34b6114125fbde1dea2f24a72fbf
 bytes32 constant REVERSE_NODE = 0xa097f6721ce401e757d1223a763fef49b8b5f90bb18567ddb86fd205dff71d34;
 // @param ADDR_REVERSE_NODE The node hash of "addr.reverse"
 bytes32 constant ADDR_REVERSE_NODE = 0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
-bytes32 constant SHA3_LOOKUP = 0x3031323334353637383961626364656600000000000000000000000000000000;
 // @param GRACE_PERIOD the grace period for expired names
 uint256 constant GRACE_PERIOD = 90 days;
 // @param BASE_ETH_NAME The dnsName of "base.eth" returned by NameEncoder.dnsEncode("base.eth")
