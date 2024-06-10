33dbe1d3ae8785e5c9366d7499d23fc83d9b94c0
katzman <steve.katzman@coinbase.com>
2024-06-10 11:32:17 -0700

    Registry config script updates

--- contracts/script/configure/EstablishBaseNamespace.s.sol
+++ contracts/script/configure/EstablishBaseNamespace.s.sol
@@ -11,14 +11,15 @@ contract EstablishNamespaces is Script {
         address deployerAddress = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
-        address ensAddress = 0xBD69dd64b94fe7435157F4851e4b4Aa3A0988c90; // deployer-owned registry
+        address ensAddress = 0x1d3C6Cf6737921c798f07Cd6469A72f173166657; // deployer-owned registry
         Registry registry = Registry(ensAddress);
+        address baseRegistrar = 0x0Cff05B4e1DF41fB5423448d4fDC81eB9Bef21df; // base registrar must own 2LD 
 
         // establish the base.eth namespace
         bytes32 ethLabel = keccak256("eth");
-        bytes32 baseLabel = keccak256("base");
+        bytes32 baseLabel = keccak256("basetest"); // basetest.eth is our sepolia test domain 
         registry.setSubnodeOwner(0x0, ethLabel, deployerAddress);
-        registry.setSubnodeOwner(ETH_NODE, baseLabel, deployerAddress);
+        registry.setSubnodeOwner(ETH_NODE, baseLabel, baseRegistrar);
 
         vm.stopBroadcast();
     }
