1920de6ab1d7f537435aea546fd10eb45072f38a
katzman <steve.katzman@coinbase.com>
2024-06-12 12:12:33 -0700

    Add testing infra

---
+++ contracts/test/L2Resolver/L2ResolverBase.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {L2Resolver} from "src/L2/L2Resolver.sol";
+import {Registry} from "src/L2/Registry.sol";
+import {ENS} from "ens-contracts/registry/ENS.sol";
+import {ETH_NODE, REVERSE_NODE} from "src/util/Constants.sol";
+
+contract L2ResolverBase is Test {
+
+    L2Resolver public resolver;
+    Registry public registry;
+    address reverse = makeAddr("reverse");
+    address controller = makeAddr("controller");
+    address owner = makeAddr("owner");
+    address user = makeAddr("user");
+
+    function setUp() public {
+        registry = new Registry(owner);
+        resolver = new L2Resolver(ENS(address(registry)), controller, reverse, owner);
+        _establishNamespace();
+    }
+
+    function _establishNamespace() internal virtual {
+        // establish the base.eth namespace
+        bytes32 ethLabel = keccak256("eth");
+        bytes32 baseLabel = keccak256("base");
+        vm.prank(owner);
+        registry.setSubnodeOwner(0x0, ethLabel, owner);
+        vm.prank(owner);
+        registry.setSubnodeOwner(ETH_NODE, baseLabel, owner);
+
+        // establish the addr.reverse namespace
+        vm.prank(owner);
+        registry.setSubnodeOwner(0x0, keccak256("reverse"), owner);
+        vm.prank(owner);
+        registry.setSubnodeOwner(REVERSE_NODE, keccak256("addr"), address(reverse));
+    }
+
+    function test_constructor() public view {
+        assertEq(address(resolver.ens()), address(registry));
+        assertEq(resolver.registrarController(), controller);
+        assertEq(resolver.reverseRegistrar(), reverse);
+        assertEq(resolver.owner(), owner);
+    }
+}
