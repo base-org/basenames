bd10713bc881282d3dd8540b1ca39b2016abb37b
katzman <steve.katzman@coinbase.com>
2024-06-13 12:31:25 -0700

    Stable Price Oracle natspec

--- src/L2/StablePriceOracle.sol
+++ src/L2/StablePriceOracle.sol
@@ -5,18 +5,42 @@ import {StringUtils} from "ens-contracts/ethregistrar/StringUtils.sol";
 
 import {IPriceOracle} from "src/L2/interface/IPriceOracle.sol";
 
-// StablePriceOracle sets a price in wei
+/// @title Stable Pricing Oracle
+///
+/// @notice The pricing mechanism for setting the "base price" of names on a per-letter basis.
+///         Inspired by the ENS StablePriceOracle contract:
+///         https://github.com/ensdomains/ens-contracts/blob/staging/contracts/ethregistrar/StablePriceOracle.sol
+///
+/// @author Coinbase (https://github.com/base-org/usernames)
+/// @author ENS (https://github.com/ensdomains/ens-contracts)
 contract StablePriceOracle is IPriceOracle {
     using StringUtils for *;
 
-    // Rent in wei by length
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          STORAGE                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    
+    /// @notice The price for a 1 letter name per second.
     uint256 public immutable price1Letter;
+
+    /// @notice The price for a 2 letter name per second. 
     uint256 public immutable price2Letter;
+
+    /// @notice The price for a 3 letter name per second.
     uint256 public immutable price3Letter;
+
+    /// @notice The price for a 4 letter name per second.
     uint256 public immutable price4Letter;
+
+    /// @notice The price for a 5 to 9 letter name per second.
     uint256 public immutable price5Letter;
+
+    /// @notice The price for a 10 or longer letter name per second.
     uint256 public immutable price10Letter;
 
+    /// @notice Price Oracle constructor which sets the immutably stored prices.
+    /// 
+    /// @param _rentPrices An array of prices orderd in increasing length. 
     constructor(uint256[] memory _rentPrices) {
         price1Letter = _rentPrices[0];
         price2Letter = _rentPrices[1];
@@ -26,13 +50,13 @@ contract StablePriceOracle is IPriceOracle {
         price10Letter = _rentPrices[5];
     }
 
-    /**
-     * @dev Returns the price to register or renew a name.
-     * @param name The name being registered or renewed.
-     * @param expires When the name presently expires (0 if this is a new registration).
-     * @param duration How long the name is being registered or extended for, in seconds.
-     * @return base premium tuple of base price + premium price
-     */
+    /// @notice Returns the price to register or renew a name given an expiry and duration.
+    ///
+    /// @param name The name being registered or renewed.
+    /// @param expires When the name presently expires (0 if this is a new registration).
+    /// @param duration How long the name is being registered or extended for, in seconds.
+    ///
+    /// @return A `Price` tuple of `basePrice` and `premiumPrice`.
     function price(string calldata name, uint256 expires, uint256 duration)
         external
         view
@@ -58,16 +82,12 @@ contract StablePriceOracle is IPriceOracle {
         return IPriceOracle.Price({base: basePrice, premium: premium_});
     }
 
-    /**
-     * @dev Returns the pricing premium in wei.
-     */
+    /// @notice Returns the pricing premium denominated in wei.
     function premium(string calldata name, uint256 expires, uint256 duration) external view returns (uint256) {
         return _premium(name, expires, duration);
     }
 
-    /**
-     * @dev Returns the pricing premium in internal base units.
-     */
+    /// @notice Returns the pricing premium denominated in wei.
     function _premium(string memory, uint256, uint256) internal view virtual returns (uint256) {
         return 0;
     }
