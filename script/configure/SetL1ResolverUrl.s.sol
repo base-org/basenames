6af86747d41a563a5d2bcac55e1e3cd2a68d51e5
katzman <steve.katzman@coinbase.com>
2024-06-14 13:28:36 -0700

    Modify the Gateway URL of our testnet L1Resolver  (#35)
    * modify script to add new URL, add broadcast record
    
    * lint

--- script/configure/SetL1ResolverUrl.s.sol
+++ script/configure/SetL1ResolverUrl.s.sol
@@ -7,10 +7,11 @@ import "src/L1/L1Resolver.sol";
 contract SetL1ResolverUrl is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address resolverAddress = 0x5F15c3B5949F5767F5Ca9013a8E4Ca4D97a053eD;
+        address resolverAddress = vm.envAddress("L1_RESOLVER_ADDR");
         vm.startBroadcast(deployerPrivateKey);
 
-        string memory NEW_URL = "";
+        string memory NEW_URL =
+            "https://api-entry-gateway-development.cbhq.net/api/v1/domain/resolver/resolveDomain/{sender}/{data}";
 
         L1Resolver resolver = L1Resolver(resolverAddress);
         resolver.setUrl(NEW_URL);
