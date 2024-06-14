c54efe3a68de7e62f54be4a949f5b949c4ccec3b
keshav <keshav17@berkeley.edu>
2024-06-14 14:01:35 -0700

    premiumprice-eda

--- src/L2/ExponentialPremiumPriceOracle.sol
+++ src/L2/ExponentialPremiumPriceOracle.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ~0.8.17;
 
 import "./StablePriceOracle.sol";
@@ -6,27 +7,24 @@ import {EDAPrice} from "src/lib/EDAPrice.sol";
 import "solady/utils/FixedPointMathLib.sol";
 import {Test, console} from "forge-std/Test.sol";
 
-
 contract ExponentialPremiumPriceOracle is StablePriceOracle {
     uint256 public immutable startPremium;
     uint256 public immutable endValue;
+    uint256 constant PRECISION = 1e18;
 
     constructor(uint256[] memory rentPrices, uint256 startPremium_, uint256 totalDays) StablePriceOracle(rentPrices) {
         startPremium = startPremium_;
         endValue = startPremium >> totalDays;
     }
-
-    uint256 constant PRECISION = 1e18;
-
     /**
      * @dev Returns the pricing premium in internal base units.
      */
+
     function _premium(string memory, uint256 expires, uint256) internal view override returns (uint256) {
         expires = expires + GRACE_PERIOD;
         if (expires > block.timestamp) {
             return 0;
         }
-
         uint256 elapsed = block.timestamp - expires;
         uint256 premium = decayedPremium(elapsed);
         if (premium > endValue) {
@@ -34,17 +32,17 @@ contract ExponentialPremiumPriceOracle is StablePriceOracle {
         }
         return 0;
     }
-
     /**
      * @dev Returns the premium price at current time elapsed
      * @param elapsed time past since expiry
      */
+
     function decayedPremium(uint256 elapsed) public view returns (uint256) {
+        /// @dev The half-life of the premium price decay
         uint256 secondsInPeriod = 1 days;
-        uint256 perPeriodDecayPercentWad = FixedPointMathLib.WAD/2;
+        /// @dev 50% decay per period in wad format
+        uint256 perPeriodDecayPercentWad = FixedPointMathLib.WAD / 2;
         uint256 premium = EDAPrice.currentPrice(startPremium, elapsed, secondsInPeriod, perPeriodDecayPercentWad);
         return premium;
     }
-
 }
-
