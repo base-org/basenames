2fe480d3ba8210c3cbc099f3fe667ddc95e011be
katzman <steve.katzman@coinbase.com>
2024-06-13 20:24:36 -0700

    Fix name of test contract

--- test/RegistrarController/DiscountedRegisterPrice.t.sol
+++ test/RegistrarController/DiscountedRegisterPrice.t.sol
@@ -5,7 +5,7 @@ import {RegistrarControllerBase} from "./RegistrarControllerBase.t.sol";
 import {RegistrarController} from "src/L2/RegistrarController.sol";
 import {IPriceOracle} from "src/L2/interface/IPriceOracle.sol";
 
-contract discountedRegisterPrice is RegistrarControllerBase {
+contract DiscountedRegisterPrice is RegistrarControllerBase {
     function test_returnsADiscountedPrice_whenThePriceIsGreaterThanTheDiscount(uint256 price) public {
         vm.assume(price > discountAmount);
         prices.setPrice(name, IPriceOracle.Price({base: price, premium: 0}));
