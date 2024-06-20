bb356ab39a8b09667d13027ab1e69cdaf5ba7b61
katzman <steve.katzman@coinbase.com>
2024-06-20 14:34:37 -0700

    Lint

--- script/configure/SetL1ResolverSigner.s.sol
+++ script/configure/SetL1ResolverSigner.s.sol
@@ -4,16 +4,19 @@ pragma solidity ^0.8.23;
 import "forge-std/Script.sol";
 import "src/L1/L1Resolver.sol";
 
-contract SetL1ResolverUrl is Script {
+contract SetL1ResolverSigner is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address resolverAddress = vm.envAddress("L1_RESOLVER_ADDR");
         vm.startBroadcast(deployerPrivateKey);
 
-        string memory NEW_URL = "https://api.coinbase.com/api/v1/domain/resolver/resolveDomain/{sender}/{data}";
+        address NEW_SIGNER = 0x0ae910AFA602F5460c4A6eDEc98A4F429901fAE2;
+        address[] memory signers = new address[](1);
+        signers[0] = NEW_SIGNER;
 
         L1Resolver resolver = L1Resolver(resolverAddress);
-        resolver.setUrl(NEW_URL);
+        console.log("connected to L1 resolver");
+        resolver.addSigners(signers);
 
         vm.stopBroadcast();
     }
