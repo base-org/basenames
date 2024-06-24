87def4b139a4acd45500d19a6a54a871ff9af1d8
katzman <steve.katzman@coinbase.com>
2024-06-24 11:24:44 -0700

    Add unit tests for ERC1155 Discount Validator

--- test/discounts/ERC1155DiscountValidator/ERC1155DiscountValidatorBase.t.sol
+++ test/discounts/ERC1155DiscountValidator/ERC1155DiscountValidatorBase.t.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.8.23;
 
 import {Test} from "forge-std/Test.sol";
 import {ERC1155DiscountValidator} from "src/L2/discounts/ERC1155DiscountValidator.sol";
+import {MockERC1155} from "test/mocks/MockERC1155.sol";
 
 contract ERC1155DiscountValidatorBase is Test {
-    
     ERC1155DiscountValidator validator;
-
+    MockERC1155 token;
+    uint256 tokenId = 1;
+    address userA = makeAddr("userA");
+    address userB = makeAddr("userB");
 
     function setUp() public {
-        validator = new ERC1155DiscountValidator();
+        token = new MockERC1155();
+        validator = new ERC1155DiscountValidator(address(token), tokenId);
     }
 }
