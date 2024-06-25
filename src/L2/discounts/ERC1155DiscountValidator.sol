95db480115dd5784a44e408a12a1bfd7321e3789
katzman <steve.katzman@coinbase.com>
2024-06-25 13:07:13 -0700

    Add clarifying comment to ERC1155DiscountValidator

--- src/L2/discounts/ERC1155DiscountValidator.sol
+++ src/L2/discounts/ERC1155DiscountValidator.sol
@@ -8,6 +8,7 @@ import {IDiscountValidator} from "src/L2/interface/IDiscountValidator.sol";
 /// @title Discount Validator for: ERC1155 NFTs
 ///
 /// @notice Implements an NFT ownership validator for a stored `tokenId` for an ERC1155 `token` contract.
+///         This discount validator should only be used for "soul-bound" tokens.
 ///
 /// @author Coinbase (https://github.com/base-org/usernames)
 contract ERC1155DiscountValidator is IDiscountValidator {
