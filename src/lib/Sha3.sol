c24df9cc6d1dd46663d77f6470985d282316be1c
katzman <steve.katzman@coinbase.com>
2024-06-25 10:26:01 -0700

    Add gas optimization to

--- src/lib/Sha3.sol
+++ src/lib/Sha3.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.23;
 
 library Sha3 {
     // Hex encoding of "0123456789abcdef"
-    bytes32 constant ALPHABET = 0x3031323334353637383961626364656600000000000000000000000000000000;
+    bytes32 constant ALPHABET = 0x30_31_32_33_34_35_36_37_38_39_61_62_63_64_65_66_00000000000000000000000000000000;
 
     /**
      * @dev An optimised function to compute the sha3 of the lower-case
@@ -17,10 +17,10 @@ library Sha3 {
             for { let i := 40 } gt(i, 0) {} {
                 i := sub(i, 1)
                 mstore8(i, byte(and(addr, 0xf), ALPHABET))
-                addr := div(addr, 0x10)
+                addr := shr(4, addr)
                 i := sub(i, 1)
                 mstore8(i, byte(and(addr, 0xf), ALPHABET))
-                addr := div(addr, 0x10)
+                addr := shr(4, addr)
             }
 
             ret := keccak256(0, 40)
