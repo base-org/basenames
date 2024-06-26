593f1705855fd334704051600f4118ed5f1f51ff
katzman <steve.katzman@coinbase.com>
2024-06-26 10:51:54 -0700

    Remove assembly, use

--- src/L2/ReverseRegistrar.sol
+++ src/L2/ReverseRegistrar.sol
@@ -183,7 +183,7 @@ contract ReverseRegistrar is Ownable {
     /// @return `true` if the address returned from `Ownable:owner()` == msg.sender, else `false`.
     function _ownsContract(address addr) internal view returns (bool) {
         // Determine if a contract exists at `addr` and return early if not
-        if (!_isContract(addr)) {
+        if (addr.code.length == 0) {
             return false;
         }
         // If a contract does exist, try and call `Ownable.owner()`
@@ -191,17 +191,6 @@ contract ReverseRegistrar is Ownable {
             return owner == msg.sender;
         } catch {
             return false;
-        }
-    }
-
-    /// @notice Check if the provided `addr` has a nonzero `extcodesize`.
-    ///
-    /// @param addr The address to check.
-    ///
-    /// @return result `true` if `extcodesize` >  0, else `false`.
-    function _isContract(address addr) internal view returns (bool result) {
-        assembly {
-            result := extcodesize(addr)
         }
     }
 }
