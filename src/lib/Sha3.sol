8c75097d4a01f2db379a8c508137cae3ffecc3a0
katzman <steve.katzman@coinbase.com>
2024-06-26 11:16:22 -0700

    Add header natspec to Sha3

--- src/lib/Sha3.sol
+++ src/lib/Sha3.sol
@@ -1,6 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+/// @title Sha3 Hex Encoding
+///
+/// @notice This method is copied from the ENS `ReverseRegistrar` contract. It's been moved to its own
+///         lib for readability and testing purposes.
+///         See: https://github.com/ensdomains/ens-contracts/blob/545a0104d0fbdd10865743e25729a921a76fd950/contracts/reverseRegistrar/ReverseRegistrar.sol#L164-L181
+///
+/// @author ENS (https://github.com/ensdomains/ens-contracts)
 library Sha3 {
     // Hex encoding of "0123456789abcdef"
     bytes32 constant ALPHABET = 0x30_31_32_33_34_35_36_37_38_39_61_62_63_64_65_66_00000000000000000000000000000000;
