6d603411025a9073450f9665520d49f737b55b1a
katzman <steve.katzman@coinbase.com>
2024-07-23 16:29:05 -0700

    [audit2] #6 Remove addr.reverse setting from ReverseRegistrar (#74)
    * Remove addr.reverse setting from ReverseRegistrar
    
    * Address PR suggestion

--- test/ReverseRegistrar/ClaimWithResolver.t.sol
+++ test/ReverseRegistrar/ClaimWithResolver.t.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.23;
 import {ReverseRegistrarBase} from "./ReverseRegistrarBase.t.sol";
 import {ReverseRegistrar} from "src/L2/ReverseRegistrar.sol";
 import {Sha3} from "src/lib/Sha3.sol";
-import {ADDR_REVERSE_NODE} from "src/util/Constants.sol";
+import {BASE_REVERSE_NODE} from "src/util/Constants.sol";
 
 contract ClaimWithResolver is ReverseRegistrarBase {
     address resolver = makeAddr("resolver");
 
     function test_allowsUser_toClaimWithResolver() public {
         bytes32 labelHash = Sha3.hexAddress(user);
-        bytes32 reverseNode = keccak256(abi.encodePacked(ADDR_REVERSE_NODE, labelHash));
+        bytes32 reverseNode = keccak256(abi.encodePacked(BASE_REVERSE_NODE, labelHash));
 
         vm.expectEmit(address(reverse));
-        emit ReverseRegistrar.ReverseClaimed(user, reverseNode);
+        emit ReverseRegistrar.BaseReverseClaimed(user, reverseNode);
         vm.prank(user);
         bytes32 returnedReverseNode = reverse.claimWithResolver(user, resolver);
         assertTrue(reverseNode == returnedReverseNode);
--- test/ReverseRegistrar/Node.t.sol
+++ test/ReverseRegistrar/Node.t.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.23;
 
 import {ReverseRegistrarBase} from "./ReverseRegistrarBase.t.sol";
 import {Sha3} from "src/lib/Sha3.sol";
-import {ADDR_REVERSE_NODE} from "src/util/Constants.sol";
+import {BASE_REVERSE_NODE} from "src/util/Constants.sol";
 
 contract Node is ReverseRegistrarBase {
     function test_returnsExpectedNode(address addr) public view {
         bytes32 labelHash = Sha3.hexAddress(addr);
-        bytes32 expectedNode = keccak256(abi.encodePacked(ADDR_REVERSE_NODE, labelHash));
+        bytes32 expectedNode = keccak256(abi.encodePacked(BASE_REVERSE_NODE, labelHash));
         bytes32 retNode = reverse.node(addr);
         assertTrue(retNode == expectedNode);
     }
