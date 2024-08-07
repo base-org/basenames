749dab1afbfcc4bef5ecc4ee55f597f9cb405a41
katzman <steve.katzman@coinbase.com>
2024-08-07 16:33:05 -0700

    Paramterize halflife, include csv output

--- py/Price.py
+++ py/Price.py
@@ -2,7 +2,8 @@ import math
 import matplotlib.pyplot as plt
 
 PRECISION = 10 ** 18
-SECONDS_PER_DAY = 86400
+SECONDS_PER_HOUR = 2600
+PER_PERIOD_DECAY_PERCENT = 50
 DAY_FRACTION = 8
 
 def decayed_premium(start_premium, elapsed_seconds, seconds_in_period, per_period_decay_percent_wad):
@@ -18,42 +19,42 @@ def calculate_prices(base_price, start_premium, end_value, num_days, seconds_in_
     elapsed_times = []
     prices = []
 
-    for day in range(num_days*DAY_FRACTION+1):
+    for hour in range(num_days * 24 + 1):
         for i in range(10):
-            if day < num_days*DAY_FRACTION:
-                elapsed_time = (day + i / 10) * SECONDS_PER_DAY
+            elapsed_time = (hour + i / 10) * SECONDS_PER_HOUR
 
-                premium = decayed_premium(start_premium, elapsed_time, seconds_in_period, per_period_decay_percent_wad)
+            premium = decayed_premium(start_premium, elapsed_time, seconds_in_period, per_period_decay_percent_wad)
 
-                # price = max(premium, end_value) / PRECISION
-                price = ((premium - end_value) / PRECISION) + base_price
-                elapsed_times.append(day + i / 10)
-                prices.append(price)
-            else:
-                elapsed_times.append(day + i / 10)
-                prices.append(base_price)
+            price = (((premium - end_value) / PRECISION)) + base_price
+            elapsed_times.append(hour + i / 10)
+            prices.append(price)
 
     return elapsed_times, prices
 
 start = int(input("Input a value for the start premium (ETH): "))
-total = int(input("How many days would it take for the price to reach its end value: "))
-num = int(input("Input a value for the number of days over which the price should decay: "))
+num_days = int(input("Input a value for the number of days over which the price should decay: "))
+halflife = float(input("Input the number of hours in the halflife: "))
 base = float(input("Input the base price (ETH) of the name: "))
 
 start_premium = start * (10 ** 18)
-total_days = total
-seconds_in_period = SECONDS_PER_DAY
-per_period_decay_percent = 50
+seconds_in_period = SECONDS_PER_HOUR * halflife
+num_hours = num_days * 24
 
-per_period_decay_percent_wad = int(per_period_decay_percent * PRECISION / 100)
-end_value = start_premium >> total_days*DAY_FRACTION
+per_period_decay_percent_wad = int(PER_PERIOD_DECAY_PERCENT * PRECISION / 100)
+end_value = start_premium >> int(num_days * (24 / halflife))
 
-num_days = num  
+print(end_value)
 
 elapsed_times, prices = calculate_prices(base, start_premium, end_value, num_days, seconds_in_period, per_period_decay_percent_wad)
-plt.figure(figsize=(start, num))
-plt.plot([x * 3 for x in elapsed_times], prices, marker='o')
-# plt.xlabel('Elapsed Time (days/%s)' % DAY_FRACTION)
+
+data = zip(elapsed_times, prices)
+
+with open("1.5hr1day.csv", "a") as f:
+    for line in data:
+        f.write(str(line[0]) + "," + str(line[1]) + "\n")
+
+plt.figure(figsize=(start, num_hours))
+plt.plot(elapsed_times, prices, marker='o')
 plt.xlabel('Elapsed Time (hours)')
 plt.ylabel('Premium Price')
 plt.title('Pricing Chart')
