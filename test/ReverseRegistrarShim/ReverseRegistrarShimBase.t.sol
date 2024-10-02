fc735bf29b9f67fee6e27241008d8674cfba62ff
katzman <steve.katzman@coinbase.com>
2024-10-02 12:02:21 -0700

    Add tests for Shim

---
+++ test/ReverseRegistrarShim/ReverseRegistrarShimBase.t.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {ReverseRegistrarShim} from "src/L2/ReverseRegistrarShim.sol";
+import {MockReverseRegistrar} from "test/mocks/MockReverseRegistrar.sol";
+import {MockReverseResolver} from "test/mocks/MockReverseResolver.sol";
+import {MockPublicResolver} from "test/mocks/MockPublicResolver.sol";
+
+contract ReverseRegistrarShimBase is Test {
+
+    MockReverseResolver revRes;
+    MockReverseRegistrar revReg;
+    MockPublicResolver resolver;
+
+    ReverseRegistrarShim public shim;
+
+    address userA;
+    address userB;
+    string nameA = "userAName";
+    string nameB = "userBName";
+
+    uint256 signatureExpiry = 0;
+    bytes signature;
+
+    function setUp() external {
+        revRes = new MockReverseResolver();
+        revReg = new MockReverseRegistrar();
+        resolver = new MockPublicResolver();
+        shim = new ReverseRegistrarShim(address(revReg), address(revRes), address(resolver));
+
+        userA = makeAddr("userA");
+        userB = makeAddr("userB");
+
+    }
+}
---
+++ test/mocks/MockReverseResolver.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract MockReverseResolver {
+    function setNameForAddrWithSignature(address, string calldata, uint256, bytes memory) external view returns (bytes32) {
+        return bytes32(block.timestamp);
+    }
+}
