2ee32ec47c2ef5c88672be8560171e42a241e8b5
tomasandroil <tomasandroil@gmail.com>
2025-01-24 20:11:46 +0100

    fix typos DeployL1Resolver.s.sol

--- script/deploy/DeployL1Resolver.s.sol
+++ script/deploy/DeployL1Resolver.s.sol
@@ -7,7 +7,7 @@ import "src/L1/L1Resolver.sol";
 contract DeployL1Resolver is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddresss = vm.addr(deployerPrivateKey);
+        address deployerAddress = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
         /// L1 Resolver constructor data
@@ -15,7 +15,7 @@ contract DeployL1Resolver is Script {
             "https://api-entry-gateway-development.cbhq.net/api/v1/subdomain/resolver/resolveDomain/{sender}/{data}"; //
         address[] memory signers = new address[](1);
         signers[0] = 0xa412c16ECd2198A6aBce8235651E105684Fb77ed; // DEV signer
-        address owner = deployerAddresss;
+        address owner = deployerAddress;
         address rootResolver = 0x8FADE66B79cC9f707aB26799354482EB93a5B7dD; //basetest.eth root resolver on sepolia
 
         L1Resolver l1 = new L1Resolver(url, signers, owner, rootResolver);
